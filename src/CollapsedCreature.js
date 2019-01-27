7d3564f78d8a4acf845cce22013bbec68fb4b9c9
Paul Ladyman <ladyman.paul@gmail.com>
2019-01-27 11:23:41 +0000

    Different dead stlyes for expanded and collapsed creatures

--- src/CollapsedCreature.js
+++ src/CollapsedCreature.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 function CollapsedCreature({creature}) {
-  const name = creature.alive ? creature.name : `${creature.name} (dead/unconscious)`;
+  const nameModifier = creature.alive ? '' : 'collapsed-creature--name__dead';
+  const classes = `collapsed-creature--name ${nameModifier}`
   return (
     <div className="collapsed-creature centered__space-between">
-      <div className="collapsed-creature--name">{name}</div>
+      <div className={classes}>{creature.name}</div>
       {creature.healthPoints && <div>{creature.healthPoints}HP</div>}
     </div>
   );
