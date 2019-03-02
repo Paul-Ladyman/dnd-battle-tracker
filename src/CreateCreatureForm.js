c68fc96eb6bf62c382eb4640123d28267d2444f6
Paul Ladyman <ladyman.paul@gmail.com>
2019-03-02 17:40:39 +0000

    Add error state to create creature form

--- src/CreateCreatureForm.js
+++ src/CreateCreatureForm.js
@@ -4,11 +4,15 @@ class CreateCreatureForm extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
+    this.initialState = {
       name: '',
       initiative: '',
       healthPoints: '',
+      nameError: false,
+      initiativeError: false
     };
+
+    this.state = this.initialState;
 
     this.nameInput = React.createRef();
 
@@ -17,13 +21,12 @@ class CreateCreatureForm extends Component {
     this.formHandler = this.formHandler.bind(this);
   }
 
-  resetForm() {
-    this.setState({
-      name: '',
-      initiative: '',
-      healthPoints: '',
-    });
+  componentDidMount() {
+    this.nameInput.current.focus();
+  }
 
+  resetForm() {
+    this.setState(this.initialState);
     this.nameInput.current.focus();
   }
 
@@ -48,22 +51,30 @@ class CreateCreatureForm extends Component {
 
         this.resetForm();
         this.props.createCreature(creature);
+      } else {
+        const nameError = state.name === '';
+        const initiativeError = state.initiative === '';
+        this.setState({...this.state, nameError, initiativeError});
       }
     }
   }
 
   render() {
-    const {name, initiative, healthPoints} = this.state;
+    const {name, initiative, healthPoints, nameError, initiativeError} = this.state;
+    const inputClass = 'create-creature-form--input';
+    const inputErrorClass = 'create-creature-form--input__error';
+    const nameClass = nameError ? `${inputClass} ${inputErrorClass}` : inputClass;
+    const initiativeClass = initiativeError ? `${inputClass} ${inputErrorClass}` : inputClass;
     return (
       <form className="create-creature-form centered__space-evenly" onKeyDown={this.formHandler}>
         <div className="create-creature-form--item">
-          <input className="create-creature-form--input" type="text" name="name" placeholder="Name" value={name} onChange={this.handleChange} ref={this.nameInput}/>
+          <input className={nameClass} type="text" name="name" placeholder="Name" value={name} onChange={this.handleChange} ref={this.nameInput}/>
         </div>
         <div className="create-creature-form--item">
-          <input className="create-creature-form--input" type="number" name="initiative" placeholder="Initiative" value={initiative} onChange={this.handleChange}/>
+          <input className={initiativeClass} type="number" name="initiative" placeholder="Initiative" value={initiative} onChange={this.handleChange}/>
         </div>
         <div className="create-creature-form--item">
-          <input className="create-creature-form--input" type="number" name="healthPoints" placeholder="HP" value={healthPoints} onChange={this.handleChange}/>
+          <input className={inputClass} type="number" name="healthPoints" placeholder="HP" value={healthPoints} onChange={this.handleChange}/>
         </div>
       </form>
     ); 
