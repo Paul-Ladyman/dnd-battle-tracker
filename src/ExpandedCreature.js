e20452bd9033bbf03685bfb3a65a4e2d80b5367a
Paul Ladyman <ladyman.paul@gmail.com>
2019-03-03 10:57:28 +0000

    Link to unconscious condition if a creature is KO'd

--- src/ExpandedCreature.js
+++ src/ExpandedCreature.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CreatureNoteList from './CreatureNoteList';
 import HealthPoints from './HealthPoints';
+import { conditionDescriptions } from './conditions';
 
 function ExpandedCreature({
   creature,
@@ -11,7 +12,11 @@ function ExpandedCreature({
   removeNoteFromCreature
 }) {
   const {alive, name, maxHealthPoints, healthPoints, initiative, id, conditions, notes} = creature;
-  const displayName = alive ? name : `${name} (dead/unconscious)`;
+  const displayName = alive ?
+    name : 
+    <React.Fragment>
+      {name} (<a href={conditionDescriptions.unconscious} target="_blank">unconscious/dead</a>)
+    </React.Fragment>;
   const showHealth = healthPoints !== undefined;
   const showConditions = conditions.length > 0;
   const showNotes = notes.length > 0;
