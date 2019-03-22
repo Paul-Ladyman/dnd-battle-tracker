e2776877490ee2f8b554735ea3ded0625f1322d0
Paul Ladyman <ladyman.paul@gmail.com>
2019-03-22 11:27:16 +0000

    Add error handling around healthPoints in create creature form. Must be > 0.

--- src/CreateCreatureForm.js
+++ src/CreateCreatureForm.js
@@ -9,7 +9,8 @@ class CreateCreatureForm extends Component {
       initiative: '',
       healthPoints: '',
       nameError: false,
-      initiativeError: false
+      initiativeError: false,
+      healthError: false
     };
 
     this.state = this.initialState;
@@ -40,31 +41,33 @@ class CreateCreatureForm extends Component {
     if (event.keyCode === 13) {
       const state = this.state;
 
-      if (state.name !== '' && state.initiative !== '') {
-        const healthPoints = state.healthPoints === '' ?
-          undefined :
-          parseInt(state.healthPoints);
+      const healthPoints = state.healthPoints === '' ?
+        undefined :
+        parseInt(state.healthPoints);
 
-        const initiative = parseInt(state.initiative);
-
-        const creature = {...state, healthPoints, initiative};
-
-        this.resetForm();
-        this.props.createCreature(creature);
-      } else {
+      if (state.name === '' || state.initiative === '' || healthPoints <= 0) {
         const nameError = state.name === '';
         const initiativeError = state.initiative === '';
-        this.setState({...this.state, nameError, initiativeError});
+        const healthError = healthPoints <= 0;
+        return this.setState({...this.state, nameError, initiativeError, healthError});
       }
+
+      const initiative = parseInt(state.initiative);
+
+      const creature = {...state, healthPoints, initiative};
+
+      this.resetForm();
+      this.props.createCreature(creature);
     }
   }
 
   render() {
-    const {name, initiative, healthPoints, nameError, initiativeError} = this.state;
+    const {name, initiative, healthPoints, nameError, initiativeError, healthError} = this.state;
     const inputClass = 'create-creature-form--input';
     const inputErrorClass = 'create-creature-form--input__error';
     const nameClass = nameError ? `${inputClass} ${inputErrorClass}` : inputClass;
     const initiativeClass = initiativeError ? `${inputClass} ${inputErrorClass}` : inputClass;
+    const healthClass = healthError ? `${inputClass} ${inputErrorClass}` : inputClass;
     return (
       <form className="create-creature-form" onKeyDown={this.formHandler}>
         <div className="create-creature-form--item">
@@ -74,7 +77,7 @@ class CreateCreatureForm extends Component {
           <input className={initiativeClass} type="number" name="initiative" placeholder="Initiative" value={initiative} onChange={this.handleChange}/>
         </div>
         <div className="create-creature-form--item">
-          <input className={inputClass} type="number" name="healthPoints" placeholder="HP (optional)" value={healthPoints} onChange={this.handleChange}/>
+          <input className={healthClass} type="number" name="healthPoints" placeholder="HP (optional)" value={healthPoints} onChange={this.handleChange}/>
         </div>
       </form>
     ); 
