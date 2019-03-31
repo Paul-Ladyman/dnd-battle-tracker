9234d737942a1f1f2ee03c03f2129f3ebb6b1d10
Paul Ladyman <ladyman.paul@gmail.com>
2019-03-31 11:23:02 +0100

    Improve collapsed creature status margin styles

--- src/CollapsedCreature.js
+++ src/CollapsedCreature.js
@@ -7,6 +7,8 @@ function CollapsedCreature({creature}) {
   const showConditions = creature.conditions.length > 0;
   const showNotes = creature.notes.length > 0;
   const showHealth = creature.healthPoints !== undefined;
+  const conditionsMarginClass = showHealth ? 'collapsed-creature--status__margin' : '';
+  const notesMarginClass = showHealth || showConditions ? 'collapsed-creature--status__margin' : '';
   return (
     <div className="collapsed-creature">
       <div className={nameClasses}>{creature.name}</div>
@@ -18,7 +20,7 @@ function CollapsedCreature({creature}) {
           className="collapsed-creature--health-points"
         />}
         {showConditions && 
-          <div className="collapsed-creature--conditions">{
+          <div className={`collapsed-creature--conditions ${conditionsMarginClass}`}>{
             creature.conditions.map((condition) => {
               return condition.text
             }).join(', ')
@@ -26,7 +28,7 @@ function CollapsedCreature({creature}) {
           </div>
         }
         {showNotes && 
-          <div className="collapsed-creature--notes">{
+          <div className={`collapsed-creature--notes ${notesMarginClass}`}>{
             creature.notes.map((note) => {
               return note.text
             }).join(', ')
