96efb1c3a1f17c9dd4a06b445c82f7ef1d2a2bda
Paul Ladyman <paul.ladyman@bbc.co.uk>
2019-03-31 12:10:10 +0100

    Make collapsed creature styling closer to Monster Manual
    * Make collapsed creature styling closer to Monster Manual
    
    * remove custom font
    
    * Improve collapsed creature status margin styles
    
    * Only capitalise first letter of notes in collapsed creature
    
    * Remove unused CSS styles. Add mobile override for collapsed creature width.
    
    * Reduce collapsed creature status margin to avoid spacing issues
    
    * Remove unnecessary class

--- src/CollapsedCreature.js
+++ src/CollapsedCreature.js
@@ -1,30 +1,41 @@
 import React from 'react';
 import HealthPoints from './HealthPoints';
 
+function commaSeparate(notes) {
+  return notes.map((note) => {
+    return `${note.text[0].toUpperCase()}${note.text.substring(1)}`;
+  }).join(', ');
+}
+
 function CollapsedCreature({creature}) {
   const nameModifier = creature.alive ? '' : 'collapsed-creature--name__dead';
   const nameClasses = `collapsed-creature--name ${nameModifier}`
   const showConditions = creature.conditions.length > 0;
   const showNotes = creature.notes.length > 0;
   const showHealth = creature.healthPoints !== undefined;
+  const conditionsMarginClass = showHealth ? 'collapsed-creature--status__margin' : '';
+  const notesMarginClass = showHealth || showConditions ? 'collapsed-creature--status__margin' : '';
   return (
     <div className="collapsed-creature">
       <div className={nameClasses}>{creature.name}</div>
-      {showHealth && <HealthPoints
-        short
-        hp={creature.healthPoints}
-        maxHp={creature.maxHealthPoints}
-        className="collapsed-creature--health-points"
-      />}
-      {showConditions && 
-        <div className="collapsed-creature--conditions">{
-          creature.conditions.map((condition) => {
-            return condition.text.substring(0, 3)
-          }).join(', ')
-          }
-        </div>
-      }
-      {showNotes && <div className="collapsed-creature--notes">Notes...</div>}
+      <div className="collapsed-creature--status">
+        {showHealth && <HealthPoints
+          short
+          hp={creature.healthPoints}
+          maxHp={creature.maxHealthPoints}
+          className="collapsed-creature--health-points"
+        />}
+        {showConditions && 
+          <div className={`collapsed-creature--notes ${conditionsMarginClass}`}>{
+            commaSeparate(creature.conditions)
+          }</div>
+        }
+        {showNotes && 
+          <div className={`collapsed-creature--notes ${notesMarginClass}`}>{
+            commaSeparate(creature.notes)
+          }</div>
+        }
+      </div>
     </div>
   );
 }
