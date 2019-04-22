61ccd2d80d22f5501dcdb78c7d53376c8aa2a387
Paul Ladyman <ladyman.paul@gmail.com>
2019-04-22 18:02:15 +0100

    Fix link in footer

--- src/Footer.js
+++ src/Footer.js
@@ -20,7 +20,7 @@ function Footer() {
       <p>
       Version {version}. See&nbsp;
       <ExternalLink
-        url="https://paul-ladyman.github.io/dnd-battle-tracker/info"
+        url="https://paul-ladyman.github.io/dnd-battle-tracker"
         text="D&D Battle Tracker Info"
       />
       &nbsp;for newer versions, more info and to download this page for offline battles.
