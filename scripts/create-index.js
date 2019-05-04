303767fa5193fbf76135b7b175969bd37d4923ff
Paul Ladyman <ladyman.paul@gmail.com>
2019-05-04 11:20:54 +0100

    SEO improvements
    * Add a main page title
    
    * Build and start app from a single html template
    
    * Ignore index.html produced for development
    
    * Add description to site metadata
    
    * Centralise main title
    
    * Bump version

---
+++ scripts/create-index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const Mustache = require('mustache');
+const htmlTemplate = fs.readFileSync(__dirname + '/../public/index.mustache.html').toString();
+
+const bundle = {
+  bundle: ''
+};
+const html = Mustache.render(htmlTemplate, bundle);
+
+fs.writeFileSync(__dirname + '/../public/index.html', html);
