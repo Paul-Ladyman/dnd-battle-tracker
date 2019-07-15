0bb0bbd5834d16a1b5d6ca05cd165d5c013323a2
Paul Ladyman <ladyman.paul@gmail.com>
2019-07-15 16:52:13 +0100

    Add dismiss error button

--- src/Errors.js
+++ src/Errors.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import RemoveIcon from './icons/RemoveIcon';
 
 export default function Errors({ errors }) {
   return (
     <div className="errors">
       {errors.map((error, i) => {
         return (
-          <div className="error" key={`error-${i}`}>{error}</div>
+          <div className="error" key={`error-${i}`}>
+            {error}
+            <button className="error--button" title="Dismiss error">
+              <RemoveIcon fill="black" />
+            </button>
+          </div>
         );
       })}
     </div>
