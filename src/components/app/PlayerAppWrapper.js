83475548e074099904f28acf26d3e34c24df9e01
Paul Ladyman <ladyman.paul@gmail.com>
2020-12-20 21:58:40 +0000

    PlayerAppWrapper lint fixes

--- src/components/app/PlayerAppWrapper.js
+++ src/components/app/PlayerAppWrapper.js
@@ -3,13 +3,13 @@ import RefreshingApolloProvider from '../../graphql/RefreshingApolloProvider';
 import PlayerApp from './PlayerApp';
 import OnlinePlayerApp from './OnlinePlayerApp';
 
-export default function PlayerAppWrapper(props) {
+export default function PlayerAppWrapper({ battleId }) {
   return (
     <RefreshingApolloProvider
       online
       OnlineView={OnlinePlayerApp}
       OfflineView={PlayerApp}
-      {...props}
+      battleId={battleId}
     />
   );
 }
