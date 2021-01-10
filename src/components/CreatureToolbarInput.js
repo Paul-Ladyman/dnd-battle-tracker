390c4061036dad0322f41ddb9acf7921719bb6d8
Paul Ladyman <ladyman.paul@gmail.com>
2021-01-10 18:58:13 +0000

    Make CreatureToolbarInput a function component

--- src/components/CreatureToolbarInput.js
+++ src/components/CreatureToolbarInput.js
@@ -1,63 +1,49 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import isHotkey from 'is-hotkey';
 import Input from './Input';
 import { hotkeys } from '../hotkeys/hotkeys';
 
-class CreatureToolbarInput extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: '' };
+export default function CreatureToolbarInput(props) {
+  const [value, setValue] = useState('');
 
-    this.formHandler = this.formHandler.bind(this);
-    this.resetForm = this.resetForm.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.submitHandler = this.submitHandler.bind(this);
-  }
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
+  const resetForm = () => {
+    setValue('');
+  };
 
-  formHandler(event) {
-    console.log('>>> hotkey', isHotkey(hotkeys.damageCreature, event));
-    if (isHotkey(hotkeys.healCreature, event)) {
-      event.preventDefault();
-      this.submitHandler(false);
-    } else if (isHotkey(hotkeys.damageCreature, event)) {
-      event.preventDefault();
-      this.submitHandler(true);
-    }
-  }
-
-  submitHandler(isLeftSubmit) {
-    const { value } = this.state;
+  const submitHandler = (isLeftSubmit) => {
     if (value) {
-      const { integer, leftSubmit, rightSubmit } = this.props;
-      this.resetForm();
+      const { integer, leftSubmit, rightSubmit } = props;
+      resetForm();
       const submittedValue = integer ? parseInt(value, 10) : value;
-      console.log('>>> left submit?', isLeftSubmit, leftSubmit);
       const func = isLeftSubmit ? leftSubmit : rightSubmit;
       func(submittedValue);
     }
-  }
+  };
 
-  resetForm() {
-    this.setState({ value: '' });
-  }
+  const formHandler = (event) => {
+    if (isHotkey(hotkeys.healCreature, event)) {
+      event.preventDefault();
+      submitHandler(false);
+    } else if (isHotkey(hotkeys.damageCreature, event)) {
+      event.preventDefault();
+      submitHandler(true);
+    }
+  };
 
-  render() {
-    const { value } = this.state;
-    return (
-      <Input
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        {...this.props}
-        value={value}
-        handleChange={this.handleChange}
-        submitHandler={this.submitHandler}
-        formHandler={this.formHandler}
-      />
-    );
-  }
+  return (
+    <Input
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+      value={value}
+      handleChange={handleChange}
+      submitHandler={submitHandler}
+      formHandler={formHandler}
+    />
+  );
 }
 
 CreatureToolbarInput.defaultProps = {
@@ -66,5 +52,3 @@ CreatureToolbarInput.defaultProps = {
   min: undefined,
   customClasses: '',
 };
-
-export default CreatureToolbarInput;
