184a635b96e81eeb49fbacb979332f884a68ec33
Paul Ladyman <ladyman.paul@gmail.com>
2021-02-07 15:09:49 +0000

    Revert changes around ErrorBoundary

--- src/components/error/ErrorBoundary.js
+++ src/components/error/ErrorBoundary.js
@@ -1,28 +1,25 @@
 import React from 'react';
 import Footer from '../page/Footer';
 import Title from '../page/Title';
-import BattleTrackerError from './BattleTrackerError';
 
 export default class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false, error: null };
+    this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error) {
-    const errorMessage = error instanceof BattleTrackerError ? error.message : null;
-    return { hasError: true, error: errorMessage };
+  static getDerivedStateFromError() {
+    return { hasError: true };
   }
 
   render() {
-    const { hasError, error } = this.state;
+    const { hasError } = this.state;
     const { children } = this.props;
     if (hasError) {
       return (
         <div className="main-footer-wrapper">
           <main className="main main__error">
             <Title error />
-            { error && <div className="error-panel">{error}</div> }
           </main>
           <Footer error />
         </div>
