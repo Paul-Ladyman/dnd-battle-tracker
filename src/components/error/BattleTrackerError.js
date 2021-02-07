4d650dea57e5b6bb4531516ca981bbb80498241f
Paul Ladyman <ladyman.paul@gmail.com>
2021-02-07 11:51:33 +0000

    Remove unnecessary constructor from BattleTrackerError

--- src/components/error/BattleTrackerError.js
+++ src/components/error/BattleTrackerError.js
@@ -1,6 +1,1 @@
-export default class BattleTrackerError extends Error {
-  constructor(message) {
-    super(message); // (1)
-    this.name = 'BattleTrackerError'; // (2)
-  }
-}
+export default class BattleTrackerError extends Error {}
