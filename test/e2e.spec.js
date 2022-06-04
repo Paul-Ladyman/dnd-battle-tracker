a0a6a032061f630a6c4fb4cb183650460a1247f5
Paul Ladyman <ladyman.paul@gmail.com>
2022-06-04 23:17:48 +0200

    Share battle in e2e test

--- test/e2e.spec.js
+++ test/e2e.spec.js
@@ -2,6 +2,25 @@ const { test, expect } = require('@playwright/test');
 
 test('basic test', async ({ page }) => {
   await page.goto('');
+
   const title = page.locator('.main-title');
   await expect(title).toHaveText('D&D Battle Tracker');
+
+  await page.fill('text=Creature Name', 'goblin');
+
+  await page.locator('role=button[name="Add creature"]').click();
+
+  await page.locator('role=button[name="Options Menu"]').click();
+
+  await page.locator('role=button[name="Enable share"]').click();
+
+  const playerPage = await page.locator('text=Player session link').click();
+
+  // await playerPage.waitForLoadState();
+
+  // const goblin = playerPage.locator('text=Goblin');
+
+  // await page.pause();
+
+  // await expect(goblin).toBeVisible();
 });
