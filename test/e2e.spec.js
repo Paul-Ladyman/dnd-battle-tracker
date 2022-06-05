6664485aa3dddbfe8eafbb30e500a61feafa0972
Paul Ladyman <ladyman.paul@gmail.com>
2022-06-05 22:56:07 +0200

    Passing e2e test for sharing a battle with a single creature

--- test/e2e.spec.js
+++ test/e2e.spec.js
@@ -3,9 +3,6 @@ const { test, expect } = require('@playwright/test');
 test('basic test', async ({ page }) => {
   await page.goto('');
 
-  const title = page.locator('.main-title');
-  await expect(title).toHaveText('D&D Battle Tracker');
-
   await page.fill('text=Creature Name', 'goblin');
 
   await page.locator('role=button[name="Add creature"]').click();
@@ -14,13 +11,8 @@ test('basic test', async ({ page }) => {
 
   await page.locator('role=button[name="Enable share"]').click();
 
-  const playerPage = await page.locator('text=Player session link').click();
+  await page.locator('text=Player session link').click();
 
-  // await playerPage.waitForLoadState();
-
-  // const goblin = playerPage.locator('text=Goblin');
-
-  // await page.pause();
-
-  // await expect(goblin).toBeVisible();
+  const goblin = page.locator('role=button', { hasText: /^goblin$/ });
+  await expect(goblin).toBeVisible();
 });
