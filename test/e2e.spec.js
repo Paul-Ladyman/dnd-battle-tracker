bdfafe461af5a14fbdee3c1076193cab80673622
Paul Ladyman <ladyman.paul@gmail.com>
2022-06-25 16:12:15 +0200

    Stabalise flaky e2e test by waiting for one creature to sync before adding another

--- test/e2e.spec.js
+++ test/e2e.spec.js
@@ -3,18 +3,23 @@ const { test, expect } = require('@playwright/test');
 test('A DM can share an existing battle with their players who receive subsequent updates', async ({ page, context }) => {
   await page.goto('');
 
-  const [dmPage] = context.pages();
-  await dmPage.fill('text=Creature Name', 'goblin');
-  await dmPage.locator('role=button[name="Add creature"]').click();
-  await dmPage.locator('role=button[name="Options Menu"]').click();
-  await dmPage.locator('role=button[name="Enable share"]').click();
-  await dmPage.locator('text=Player session link').click();
-  await dmPage.fill('text=Creature Name', 'owlbear');
-  await dmPage.locator('role=button[name="Add creature"]').click();
+  await page.fill('text=Creature Name', 'goblin');
+  await page.locator('role=button[name="Add creature"]').click();
+  await page.locator('role=button[name="Options Menu"]').click();
+  await page.locator('role=button[name="Enable share"]').click();
 
-  const playerPage = context.pages()[1];
+  const [playerPage] = await Promise.all([
+    context.waitForEvent('page'),
+    page.locator('text=Player session link').click(),
+  ]);
+  await playerPage.waitForLoadState();
+
   const goblin = playerPage.locator('role=button', { hasText: /^goblin$/ });
-  const owlbear = playerPage.locator('role=button', { hasText: /^owlbear$/ });
   await expect(goblin).toBeVisible();
+
+  await page.fill('text=Creature Name', 'owlbear');
+  await page.locator('role=button[name="Add creature"]').click();
+
+  const owlbear = playerPage.locator('role=button', { hasText: /^owlbear$/ });
   await expect(owlbear).toBeVisible();
 });
