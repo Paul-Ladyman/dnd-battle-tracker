ad9da8527d2ea62710677a79d610a8b107f2d5b2
Paul Ladyman <ladyman.paul@gmail.com>
2022-08-06 18:05:44 +0200

    Add assertion that player session link is displayed when share is enabled

--- test-integration/integration-share.test.js
+++ test-integration/integration-share.test.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { nanoid } from 'nanoid';
 import userEvent from '@testing-library/user-event';
 import DungeonMasterAppWrapper from '../src/components/app/DungeonMasterAppWrapper';
+import 'cross-fetch/polyfill';
 
-jest.mock('@aws-sdk/client-cognito-identity');
+jest.mock('nanoid');
 
 describe('Battle Share', () => {
   test('displays a link to the player app', async () => {
+    nanoid.mockReturnValue('random-battle-id');
     render(<DungeonMasterAppWrapper />);
     const user = userEvent.setup();
     const optionsMenu = await screen.findByRole('button', { name: 'Options Menu' });
@@ -15,9 +18,7 @@ describe('Battle Share', () => {
     const enableShare = await screen.findByRole('button', { name: 'Enable share' });
     await user.click(enableShare);
 
-    // console.log(container.textContent);
-
-    // const playerSessionLink = await screen.findByText(/Player session link/);
-    // expect(playerSessionLink).toBeVisible();
+    const playerSessionLink = await screen.findByText('Player session link random-battle-id');
+    expect(playerSessionLink).toBeVisible();
   });
 });
