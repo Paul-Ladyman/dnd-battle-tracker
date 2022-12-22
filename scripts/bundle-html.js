6b0117a226e813fcbe6d5a94c349dbb7bb793a43
Paul Ladyman <ladyman.paul@gmail.com>
2022-12-22 14:30:55 +0100

    Update ci/cd pipeline to deploy static assets with main html file

--- scripts/bundle-html.js
+++ scripts/bundle-html.js
@@ -1,15 +1,20 @@
 const fs = require('fs');
+// eslint-disable-next-line import/no-extraneous-dependencies
 const Mustache = require('mustache');
-const js = fs.readFileSync(__dirname + '/../dist/main.js').toString();
-const css = fs.readFileSync(__dirname + '/../dist/main.css').toString();
-const htmlTemplate = fs.readFileSync(__dirname + '/../public/index.mustache.html').toString();
+
+const js = fs.readFileSync(`${__dirname}/../dist/main.js`).toString();
+const css = fs.readFileSync(`${__dirname}/../dist/main.css`).toString();
+const htmlTemplate = fs.readFileSync(`${__dirname}/../public/index.mustache.html`).toString();
 
 const buildTime = new Date().getTime();
 
 const bundle = {
   bundle: `<script>window.BUILD_TIME=${buildTime}</script><script>${js}</script>`,
-  styleBundle: `<style type="text/css">${css}</style>`
+  styleBundle: `<style type="text/css">${css}</style>`,
 };
 const html = Mustache.render(htmlTemplate, bundle);
 
-fs.writeFileSync(__dirname + '/../dist/dnd-battle-tracker.html', html);
+fs.mkdirSync(`${__dirname}/../dist-final`, { recursive: true });
+fs.writeFileSync(`${__dirname}/../dist-final/dnd-battle-tracker.html`, html);
+fs.copyFileSync(`${__dirname}/../public/favicon.png`, `${__dirname}/../dist-final/favicon.png`);
+fs.copyFileSync(`${__dirname}/../public/sw.js`, `${__dirname}/../dist-final/sw.js`);
