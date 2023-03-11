e167743890fccb0cbd94aacabe3ad378372df371
Paul Ladyman <ladyman.paul@gmail.com>
2023-03-11 09:53:44 +0100

    Correctly display initiative tool for locked creatures on battle reset

--- src/components/creature/toolbar/InitiativeTool.js
+++ src/components/creature/toolbar/InitiativeTool.js
@@ -8,7 +8,8 @@ export default function InitiativeTool({
   initiative,
   addInitiativeToCreature,
 }) {
-  return initiative === null && (
+  const showTool = initiative === undefined || initiative === null;
+  return showTool && (
     <CreatureToolbarInput
       customClasses="creature-toolbar--last"
       integer
