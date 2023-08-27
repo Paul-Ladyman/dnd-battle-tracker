b817ad26f7e1554a10a62bf5c6d2bfc24f19d5ab
Paul Ladyman <ladyman.paul@gmail.com>
2023-08-27 09:42:45 +0200

    Add keys to meter slots

--- src/components/creature/SpellSlotMeters.js
+++ src/components/creature/SpellSlotMeters.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import maxSpellSlots from '../../domain/spellSlots';
 
-function FilledSpellSlot() {
-  return <div className="spell-slot-meter__filled" />;
-}
-
-function EmptySpellSlot() {
-  return <div className="spell-slot-meter__empty" />;
-}
-
 function isFull(total, used, level) {
   if (total !== undefined) {
     return total === used;
@@ -17,17 +9,22 @@ function isFull(total, used, level) {
   return maxSpellSlots[level] === used;
 }
 
+function meter(max, now) {
+  return Array.from({ length: max }).reduce((slots, _, i) => {
+    const key = slots.length;
+    const className = i < now ? 'spell-slot-meter__filled' : 'spell-slot-meter__empty';
+    const slot = <div className={className} key={key} />;
+    return [...slots, slot];
+  }, []);
+}
+
 export default function SpellSlotMeters({ totalSpellSlots, usedSpellSlots, id }) {
   const spellSlotMeters = Object.keys(maxSpellSlots).map((level) => {
     const total = totalSpellSlots?.[level];
     const used = usedSpellSlots?.[level];
     const max = total || used || 0;
     if (max === 0) return null;
-
     const now = used || 0;
-
-    const usedMeter = new Array(now).fill(<FilledSpellSlot />);
-    const remainingMeter = new Array(max - now).fill(<EmptySpellSlot />);
 
     const fullClass = isFull(total, used, level) ? 'spell-slot-meter--label__full' : '';
     const slotsId = `${id}-spell-slots-${level}`;
@@ -35,8 +32,7 @@ export default function SpellSlotMeters({ totalSpellSlots, usedSpellSlots, id })
       <div key={level} className="spell-slot-meter--container">
         <div className={`spell-slot-meter--label ${fullClass}`} id={slotsId}>{`${level} Level`}</div>
         <div className="spell-slot-meter" role="meter" aria-valuemin="0" aria-valuemax={max} aria-valuenow={now} aria-labelledby={slotsId}>
-          {usedMeter}
-          {remainingMeter}
+          {meter(max, now)}
         </div>
       </div>
     );
