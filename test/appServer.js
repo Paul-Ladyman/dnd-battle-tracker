f0a2bfb4258d2068adfbb8026d7a4917cf4fd239
Paul Ladyman <ladyman.paul@gmail.com>
2024-06-29 10:53:19 +0200

    Correct name of dist file in e2e tests

--- test/appServer.js
+++ test/appServer.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/../dist/dndbattletracker.html'));
+  res.sendFile(path.join(__dirname, '/../dist/dnd-battle-tracker.html'));
 });
 app.use(express.static('dist'));
 
