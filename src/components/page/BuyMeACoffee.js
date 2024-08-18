e4c327718ee62c9e50dfcadeb19102e159ae9bf1
Paul Ladyman <ladyman.paul@gmail.com>
2024-08-18 17:49:58 +0200

    Kofi image width

--- src/components/page/BuyMeACoffee.js
+++ src/components/page/BuyMeACoffee.js
@@ -5,5 +5,5 @@ const version = Math.floor(Math.random() * 5) + 1;
 export default function BuyMeACoffee({ mobileOnly }) {
   const className = mobileOnly ? 'buymeacoffee__mobileonly' : 'buymeacoffee__desktoponly';
   const src = `https://cdn.ko-fi.com/cdn/kofi${version}.png?v=3`;
-  return <a href="https://ko-fi.com/R5R12KANF" target="_blank" rel="noreferrer" className={className}><img height="73" style={{ border: '0px', height: '36px' }} src={src} border="0" alt="Buy Me a Coffee at ko-fi.com" /></a>;
+  return <a href="https://ko-fi.com/R5R12KANF" target="_blank" rel="noreferrer" className={className}><img width="286" height="73" style={{ border: '0px', height: '36px', width: '141px' }} src={src} border="0" alt="Buy Me a Coffee at ko-fi.com" /></a>;
 }
